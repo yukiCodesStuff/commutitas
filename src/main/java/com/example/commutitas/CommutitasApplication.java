082fe21575a6918fdc149fe1d471894d67cc028a
yukiCodesStuff <yuki.janvier@gmail.com>
2023-09-16 12:10:40 -0500

    first commit

---
+++ src/main/java/com/example/commutitas/CommutitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.commutitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommutitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/commutitas/CommutitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commutitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommutitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
